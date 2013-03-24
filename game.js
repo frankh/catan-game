c66b678221ef37d4c01bf5858ea79dedec2214b5
Frank Hamand <frankhamand@gmail.com>
2013-03-24 02:40:29 +0000

    add text resizing

--- game.js
+++ game.js
@@ -14,6 +14,7 @@ var resize = function() {
 
 	var hex_width = $('.game_hex').width();
 	var hex_height = $('.game_hex').height();
+	var font_size = hex_height / 5;
 
 	var width_offset = hex_width * (0.05) + 1;
 	var height_offset = hex_height * (0.50);
@@ -27,12 +28,34 @@ var resize = function() {
 
 	$('.hex_grid').css('left', 5.7 * width_offset+'px');
 	$('.hex_grid').css('top', 1 * height_offset+'px');
+
+	$('.hex_text').each(function() {
+		var two_chars = $(this).text().trim().length == 2;
+		
+		var font_offset_x = font_size * 0.3;
+
+		if( two_chars ) {
+			font_offset_x = font_size * 0.5;
+		}
+
+		$(this).css('top',  hex_height * 0.5 - font_size * 0.65 +'px');
+		$(this).css('left',  hex_width * 0.5 - font_offset_x +'px');
+		$(this).css('font-size',  font_size+'px');
+	});
 };
 
 $(window).load(function() {
+	for( var i = 1; i < 5; i++ ) {
+		$('.game_hex_row.template td.template')
+			.clone()
+			.removeClass('template')
+			.appendTo($('.game_hex_row.template'));
+	}
+	$('.game_hex_row.template td.template').removeClass('template');
+
 	for( var i = 0; i < 5; i++ ) {
 		$('.game_hex_row.template')
-			.clone(true)
+			.clone()
 			.removeClass('template')
 			.appendTo($('.hex_grid'));
 	}
