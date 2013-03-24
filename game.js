d24e70e6877e4eca16abae3cdbdaab4de1016639
Frank Hamand <frankhamand@gmail.com>
2013-03-24 02:06:18 +0000

    center grid in play area

--- game.js
+++ game.js
@@ -12,20 +12,21 @@ var resize = function() {
 
 	$('.game_window').width(height*(4/3));
 
-	var ratio = $('.game_board').width() / $('.game_board').height();
-
-	$('.game_current_player_notification').text(ratio);
 	var hex_width = $('.game_hex').width();
 	var hex_height = $('.game_hex').height();
 
-	$('td:nth-child(odd) .game_hex').css('top', hex_height * (0.50) + 'px');
-
 	var width_offset = hex_width * (0.05) + 1;
+	var height_offset = hex_height * (0.50);
+
+	$('td:nth-child(odd) .game_hex').css('top', height_offset + 'px');
 
 	$('td:nth-child(odd) .game_hex').css('left', width_offset + 'px');
 	$('td:nth-child(3) .game_hex').css('left', -1 * width_offset + 'px');
 	$('td:nth-child(4) .game_hex').css('left', -2 * width_offset + 'px');
 	$('td:nth-child(5) .game_hex').css('left', -3 * width_offset + 'px');
+
+	$('.hex_grid').css('left', 5.7 * width_offset+'px');
+	$('.hex_grid').css('top', 1 * height_offset+'px');
 };
 
 $(window).load(function() {
