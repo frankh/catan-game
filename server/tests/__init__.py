33391752312b39b20ef8905723acf289670448bf
Frank Hamand <fhamand@geneity.co.uk>
2013-04-08 15:49:18 +0100

    fix board generation test

--- server/tests/__init__.py
+++ server/tests/__init__.py
@@ -1,4 +1,11 @@
 import sys
 import os
+import unittest
 
-sys.path.insert(0,os.path.abspath(__file__+"/.."))
+sys.path.insert(0,os.path.abspath(__file__+"/../.."))
+
+from board_generation_tests import *
+from game_tests import *
+
+if __name__ == '__main__':
+        unittest.main()
