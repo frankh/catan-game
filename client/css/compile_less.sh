81e0a9338bd96eee6fb18f1c399fafcfae5e0233
Frank Hamand <frankhamand@gmail.com>
2013-04-20 12:33:33 +0100

    add less compilation

---
+++ client/css/compile_less.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+echo "Compiling all LESS files to CSS"
+for file in *.less
+do
+    FROM=$file
+    TO=${file/.*/.css}
+    echo "$FROM --> $TO"
+    lessc $FROM $TO -x --yui-compress
+done
