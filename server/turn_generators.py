97f873588898aef5c52579b645da74839f2a511e
Frank Hamand <frank.hamand@factorymedia.com>
2013-11-07 14:30:43 +0000

    fix vertex id in steal_from

--- server/turn_generators.py
+++ server/turn_generators.py
@@ -228,7 +228,7 @@ def move_robber(self):
 			'type': 'steal_from',
 			'locations': [{
 				'type': 'vertex',
-				'id': list(vertex.id)
+				'id': list(vertex.str_id)
 			} for vertex in target_vertices]
 		}]
 
